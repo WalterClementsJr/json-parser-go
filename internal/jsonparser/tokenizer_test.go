4bcaafa3b1710ba75a6d64006f9d24291e7a72e8
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-02-05 00:45:30 +0700

    refactor structure

--- internal/jsonparser/tokenizer_test.go
+++ internal/jsonparser/tokenizer_test.go
@@ -1,4 +1,4 @@
-package parser
+package jsonparser
 
 import (
 	"testing"
--- main.go
+++ main.go
@@ -1,24 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/walterclementsjr/json-parser-go/internal/parser"
-)
-
 func main() {
-	json := `[null, 10, "abc", [], {}]`
-	fmt.Println(json)
-
-	tokens := parser.Tokenize(json)
-	fmt.Println("Tokenizer result", tokens)
-	fmt.Println()
-
-	parseResult := parser.Parse(tokens)
-
-	switch res := parseResult.(type) {
-	case []any:
-		fmt.Println(res)
-	case map[string]any:
-	}
 }
